88f08498382c43100a9a29065fa0539f104531a4
Tamim Ahasan RIjon <tamimahasan.ar@gmail.com>
2025-07-20 13:34:33 +0600

    Add initial implementation of longest common substring algorithm

---
+++ Task #1/lcs.js
@@ -0,0 +1,14 @@
+let a = process.argv.slice(2);
+if (!a.length) console.log();
+else {
+  let s = a[0], n = s.length;
+  for (let l = n; l > 0; l--)
+    for (let i = 0; i + l <= n; i++) {
+      let sub = s.slice(i, i + l);
+      if (a.every(x => x.includes(sub))) {
+        console.log(sub);
+        process.exit();
+      }
+    }
+  console.log();
+}
